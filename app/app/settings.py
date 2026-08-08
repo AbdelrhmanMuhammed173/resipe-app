c4c10855a6fb6bb41590dd8a85c934099827f007
Abdelrhman <dodoelmagic2013@gmail.com>
2026-08-08 07:00:54 +0300

    fix docker login

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = ''
+SECRET_KEY = 'adfasvv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
